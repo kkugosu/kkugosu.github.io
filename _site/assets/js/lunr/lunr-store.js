5037d62f2e7edfc2d5747b20e07ddf1d02c5d755
hajongsu <hjs9402@gmail.com>
2019-06-10 00:20:23 +0900

    lastv

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "asdf",
+        "excerpt":"adsfasdf  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/asdf/",
+        "teaser":"http://localhost:4000/assets/images/abstract-1.jpg"}]
